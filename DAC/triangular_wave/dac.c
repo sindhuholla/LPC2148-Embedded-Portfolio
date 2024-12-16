52d0ecd81b9ef41f05a92c8065738c6235dec820
sindhuholla <80588189+sindhuholla@users.noreply.github.com>
2024-12-16 19:11:56 +0530

    Create dac.c

---
+++ DAC/triangular_wave/dac.c
@@ -0,0 +1,25 @@
+/*
+This program generates Triangle wave
+*/
+/****************************************************
+
+Submission Details:
+Name: Sindhu Holla
+File: DAC file
+
+Clock Settings:
+	FOSC	>>	12MHz (onboard)
+	PLL		>>	M=5, P=2
+	CCLK	>>  60MHz
+	PCLK	>>  15MHz 
+		
+Hardware Setup:-
+P0.25 -> Oscilloscope (A)
+
+*****************************************************/
+#include "header.h"
+
+// Initialize DAC on P0.25
+void initDAC(void) {
+    PINSEL1 |= (1 << 19);  // Configure P0.25 as DAC output (bits 18-19 in PINSEL1)
+}
